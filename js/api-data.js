d4150a8b1e68acc94886581fe8c104495d21f69a
neberpa <nestorebernalpardo@gmail.com>
2023-07-30 20:57:29 -0500

    creacion del input

--- js/api-data.js
+++ js/api-data.js
@@ -1,4 +1,7 @@
 let ts="1690645644425";
 let publicKey="2e7fa61f56b352647e0fdef97df1fc85";
 let hashVal="8d69381d9cc789006272a0bb41af1bf0";
+   
+
+//let publicKey="2e7fa61f56b352647e0fdef97df1fc85";
 
